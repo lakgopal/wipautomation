7c7afa517d791f192cc4b31547f43cbf49e4a41d
Lakshmi Gopalakrishna <lakshmigopalakrishna@Nithyashrees-MacBook-Pro.local>
2017-02-11 21:05:04 -0800

    Declaring Variables and String concatenation

---
+++ src/com/wip/automation/excercise1/VarDeclare.java
@@ -0,0 +1,27 @@
+package com.wip.automation.excercise1;
+
+public class VarDeclare {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    int week= 7;
+    double pizza=7.86;
+    char letter='I';
+    String name="Lakshmi";
+    int daysoff=2;
+    int namelength= name.length();
+    int apps=10;
+    int pages=10;
+    System.out.println("Week has:" +week+ "days");
+    System.out.println("Each pizza slice is:" +pizza+ "dollars");
+    System.out.println("First letter of India is:" +letter);
+    System.out.println("My name is:" +name);
+    System.out.println("I took," +daysoff+ " daysoff");
+    System.out.println("Length of my name is:" +namelength+ "letters");
+    System.out.println(+apps+ ", installed in my phone");
+    System.out.println("So far,I read " +pages+ " pages");
+
+
+  }
+
+}
