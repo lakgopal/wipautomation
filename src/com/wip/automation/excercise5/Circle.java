c0d44aa6ccb1dce6f41a16c99346da5719d5490f
Lakshmi Gopalakrishna <lakshmigopalakrishna@Nithyashrees-MacBook-Pro.local>
2017-02-14 22:35:42 -0800

    Circle class

---
+++ src/com/wip/automation/excercise5/Circle.java
@@ -0,0 +1,16 @@
+package com.wip.automation.excercise5;
+
+public class Circle {
+  
+  public double area(int r){
+    double PI=  3.14159;
+    return PI*r*r; 
+  }
+  
+  public double perimeter(int r){
+    double PI=  3.14159;
+    return 2*PI*r; 
+  }
+  
+
+}
