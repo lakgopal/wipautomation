9d08f47f71d377c06947f43958c7d7328d568cd6
Lakshmi Gopalakrishna <lakshmigopalakrishna@Nithyashrees-MacBook-Pro.local>
2017-02-14 22:36:57 -0800

    Main class for circle

---
+++ src/com/wip/automation/excercise5/MainCircle.java
@@ -0,0 +1,16 @@
+package com.wip.automation.excercise5;
+
+public class MainCircle {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    
+    Circle cir=new Circle();
+    double cirarea = cir.area(3);
+    double perimeter=cir.perimeter(2);
+    System.out.println("The area of circle is:" +cirarea);
+    System.out.println("The perimeter circle is:" +perimeter);
+
+  }
+
+}
