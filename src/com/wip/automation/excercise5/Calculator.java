fe63f42fcc25f4a8b0c25abf9ca4bc9da1764732
Lakshmi Gopalakrishna <lakshmigopalakrishna@Nithyashrees-MacBook-Pro.local>
2017-02-14 22:31:57 -0800

    Calculator class

---
+++ src/com/wip/automation/excercise5/Calculator.java
@@ -0,0 +1,40 @@
+package com.wip.automation.excercise5;
+
+public class Calculator {
+
+  public int add(int num1, int num2) {
+    return num1 + num2;
+
+  }
+
+  public int modulas(int num1, int num2) {
+    int mod = num1 % num2;
+    return mod;
+  }
+
+  public int substract(int num1, int num2) {
+    int sub = num1 - num2;
+    return sub;
+  }
+
+  public int multiply(int num1, int num2) {
+    int mul = num1 * num2;
+    return mul;
+  }
+
+  public int division(int num1, int num2) {
+    int div = num1 / num2;
+    return div;
+  }
+
+  public int square(int num1) {
+    int square = num1 * num1;
+    return square;
+  }
+
+  public int Add(int num1, int num2) {
+    int sum = num1 + num2;
+    return sum;
+  }
+
+}
