1838cd3acf72de6571db4b3b5d6f1d68a7c58bdd
Lakshmi Gopalakrishna <lakshmigopalakrishna@Nithyashrees-MacBook-Pro.local>
2017-02-15 21:49:49 -0800

    Modified Cicle class

--- src/com/wip/automation/excercise5/Circle.java
+++ src/com/wip/automation/excercise5/Circle.java
@@ -1,16 +1,22 @@
 package com.wip.automation.excercise5;
 
 public class Circle {
-  
-  public double area(int r){
-    double PI=  3.14159;
-    return PI*r*r; 
+  double PI = 3.14159;
+  public static int ctr = 0;
+  int radius;
+
+  public Circle(int rad) {
+    radius = rad;
+    ctr++;
   }
-  
-  public double perimeter(int r){
-    double PI=  3.14159;
-    return 2*PI*r; 
+
+  public double area() {
+    return PI * radius * radius;
   }
-  
+
+  public double perimeter() {
+    return 2 * PI * radius;
+  }
+
 
 }
